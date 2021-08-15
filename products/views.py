8f4cd74d236b6434bbe30a14ab7eba70a2ed3602
ipavka_work <ipavka@ukr.net>
2021-08-15 16:34:35 +0300

    Lesson_2

--- products/views.py
+++ products/views.py
@@ -1,29 +1,24 @@
 from django.shortcuts import render
 import json
-
+from datetime import datetime
 
 # не знаю как верно, тут оставить или в функции разместить
-with open('./products/fixtures/goods.json', 'r', encoding='utf-8') as file:
+with open('./products/fixtures/products.json', 'r', encoding='utf-8') as file:
     data_prod = json.load(file)
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        "title": "geekShop"
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
     context = {
-        'title': 'GeekShop - Каталог',
+        'title': 'geekShop - Каталог',
+        "date": datetime.now(),
         'products': data_prod
     }
     return render(request, 'products/products.html', context)
 
-
-# def products(request):
-#     context = {
-#         'title': 'GeekShop - Каталог',
-#         'header': 'Добро пожаловать на сайт!',
-#         'username': 'Иван Иванов',
-#         'products': data_prod
-#     }
-#     return render(request, 'products/products.html', context)
