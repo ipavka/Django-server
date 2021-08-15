79a831220ea5788f21ac91e1817c8c37c7a54c2f
ipavka_work <ipavka@ukr.net>
2021-08-15 13:46:10 +0300

    add static

--- products/views.py
+++ products/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render
 import json
 
 
+# не знаю как верно, тут оставить или в функции разместить
+with open('./products/fixtures/goods.json', 'r', encoding='utf-8') as file:
+    data_prod = json.load(file)
+
+
 def index(request):
     return render(request, 'products/index.html')
 
 
 def products(request):
-    with open('./products/fixtures/goods.json', 'r', encoding='utf-8') as file:
-        data_prod = json.load(file)
-    context = {
-        'title': 'GeekShop - Каталог',
-        'header': 'Добро пожаловать на сайт!',
-        'username': 'Иван Иванов',
-        'products': data_prod
-    }
+    context = {'title': 'GeekShop - Каталог'}
     return render(request, 'products/products.html', context)
+
+
+# def products(request):
+#     context = {
+#         'title': 'GeekShop - Каталог',
+#         'header': 'Добро пожаловать на сайт!',
+#         'username': 'Иван Иванов',
+#         'products': data_prod
+#     }
+#     return render(request, 'products/products.html', context)
