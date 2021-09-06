caad4e8ba11215bcf37453878b1d5b72580d1a6a
ipavka <ipavka@ya.ru>
2021-09-06 18:40:36 +0300

    make 1-2 task

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,6 @@ app_name = 'products'
 
 urlpatterns = [
     path('', products, name='product'),
+    path('<int:category_id>/', products, name='category'),
+    path('page/<int:page>/', products, name='page'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from datetime import datetime
 from products.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
@@ -10,12 +11,22 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None, page=1):
     context = {
         'title': 'geekShop - Каталог',
         "date": datetime.now(),
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all()
+        'categories': ProductCategory.objects.all(),
     }
+    product = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    paginator = Paginator(product, per_page=3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context['products'] = products_paginator
+
     return render(request, 'products/products.html', context)
 
